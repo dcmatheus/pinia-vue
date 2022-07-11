abb6bb8b8bfadb02e053fc7e99e3f3b2a72466df
dcmatheus <mathws.costag@gmail.com>
2022-07-11 10:27:30 -0300

    Cria a Sore do pinia

---
+++ src/store/main.ts
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia';
+
+export const useMainStore = defineStore('main', {
+  state: () => ({
+    counter: 0,
+  }),
+  actions: {
+    increment() {
+      this.counter += 1;
+    },
+  },
+});
+
+export default {};
